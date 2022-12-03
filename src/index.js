c6223e2cdc4428cce7466230f0e18e12a77df280
Nikhil Singh <singh789@gmail.com>
2022-12-03 12:29:17 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <>
+  <h1>Netflix Series</h1>
+  <p>Shows Name</p>
+  <ol>
+    <li>Breaking Bad</li>
+    <li>Prison Break</li>
+    <li>GOT</li>
+    <li>Dark</li>
+    <li>Friend</li>
+  </ol>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
